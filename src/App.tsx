262ded3cd64383d68e898fe46654348ac44e7e08
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2021-10-19 16:18:52 +0900

    [refactor] Redux Provider を index.tsx に移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import { createTheme, colors } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
@@ -7,7 +6,6 @@ import { initReactI18next } from 'react-i18next';
 import 'react-toastify/dist/ReactToastify.css';
 
 import jaJson from './locales/ja.json';
-import store from './stores/root';
 import Router from './Router';
 
 i18n.use(initReactI18next).init({
@@ -26,12 +24,10 @@ const AppTheme = createTheme({
 });
 
 const App = () => (
-  <Provider store={store}>
-    <ThemeProvider theme={AppTheme}>
-      <Router />
-      <ToastContainer autoClose={6000} closeButton={false} draggable={false} position="top-center" />
-    </ThemeProvider>
-  </Provider>
+  <ThemeProvider theme={AppTheme}>
+    <Router />
+    <ToastContainer autoClose={6000} closeButton={false} draggable={false} position="top-center" />
+  </ThemeProvider>
 );
 
 export default App;
